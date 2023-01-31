a155f4b116f03c89d32db911066626159b6f33a0
bhyravarapu-anjani <bhyravarapuanjani@gamil.com>
2023-01-31 16:12:51 +0530

    project archectire complected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+  
     
   ],
   imports: [
